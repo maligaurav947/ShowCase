46982184092aee20d3225d9b84c2e14434628281
Gaurav Mali <maligaurav947@gmail.com>
2022-06-12 10:03:56 +0530

    update 0.8

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //import Typed from "typed.js";
 
 var typed = new Typed(".auto-type", {
-  strings: ["Mali Gaurav", "Мали Гаурав", "ਮਾਲੀ ਗੌਰਵ", "ମାଲି ଗ aur ରବ |"],
+  strings: ["HTML", "CSS", "JavaScript"],
   typeSpeed: 150,
   loop: true,
   backSpeed: 150,
